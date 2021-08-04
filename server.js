8ca078e50466f15540591e8172d64ee033677492
Nicholas Lingad <lingadnicholas@engineering.ucla.edu>
2021-08-03 20:05:43 -0700

    first commit

--- server.js
+++ server.js
@@ -22,3 +22,59 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+var io = require('socket.io')(server)
+var chatRoomData = []
+var connectedClients = {} 
+
+io.on('connection', (client) => {
+  console.log("New client connected") 
+
+  // Client sent a message
+  client.on("SendMessage", (messageData) => {
+    chatRoomData.push(messageData)
+    sendUpdatedChatRoomData(client) 
+  })
+
+  // Client entered the chat room 
+  client.on("UserEnteredRoom", (userData) => {
+    var enteredRoomMessage = {message: `${userData.username} has entered the chat`, username: "", userID: 0, timestamp:null};
+    chatRoomData.push(enteredRoomMessage)
+    sendUpdatedChatRoomData(client)
+    connectedClients[client.id] = userData
+  })
+
+  // Creating identity for new connected user
+  client.on("CreateUserData", () => {
+    let userID = uuid(); 
+    let username = uniqueNamesGenerator({ dictionaries: [adjectives, names] }); 
+    var userData = {userID: userID, username: username}
+    client.emit("SetUserData", userData) 
+
+  })
+
+  // Player disconnecting from chat room
+  client.on('disconnecting', (data) => {
+    console.log("Client disconnecting...");
+
+    if(connectedClients[client.id]) {
+      var leftRoomMessage = {message: `${connectedClients[client.id].username} has left the chat`, username: "", userID: 0, timestamp: null};
+      chatRoomData.push(leftRoomMessage)
+      sendUpdatedChatRoomData(client) 
+      delete connectedClients[client.id]
+    }
+  })
+
+  // Clearing chat room data from server
+  client.on('ClearChat', () => { 
+    chatRoomData=[] 
+    console.log(chatRoomData)
+    sendUpdatedChatRoomData(client) 
+  })
+})
+
+// Sending update chat room data to all connected clients 
+function sendUpdatedChatRoomData(client) {
+  client.emit("RetrieveChatRoomData", chatRoomData)
+  client.broadcast.emit("RetrieveChatRoomData", chatRoomData)
+}
